95113fd5f4f021eb5f0dd306bfb882672342db36
Dany Gal <crazyio2005@gmail.com>
2026-08-08 10:41:49 +0300

    mail

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,12 @@
 <?php
 
+// Disable per-call mail logging (server's mail.log path is read-only)
+ini_set('mail.log', '');
+
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "ashik.mdashikurrahman@gmail.com" );
+define( "RECIPIENT_NAME", "FlowOne" );
+define( "RECIPIENT_EMAIL", "haflaa4u@gmail.com" );
+define( "FROM_EMAIL", "no-reply@flowone.fit" );
 
 // Read the form values
 $success = false;
@@ -15,7 +19,8 @@ $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subj
 // If all values exist, send the email
 if ( $senderName && $senderEmail && $message ) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
+  $headers  = "From: " . $senderName . " <" . FROM_EMAIL . ">\r\n";
+  $headers .= "Reply-To: " . $senderEmail . "\r\n";
   $mailBody = 'Sender Name: ' . $senderName. "\r\n" . 'Sender Email: ' . $senderEmail . "\r\n" .'Sender Phone: '. $senderPhone . "\r\n" . 'Subject: ' . $subject . "\r\n" . 'Message: ' . $message;
   $success = mail( $recipient, $subject, $mailBody, $headers );
   echo "<p class='success'>Thanks for contacting us. We will contact you ASAP!</p>";
